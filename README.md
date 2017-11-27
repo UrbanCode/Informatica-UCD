f22ddd5c5dc771befd56a8403c6103dc33f4df5a
Leo Selvaraj <leosraj@users.noreply.github.com>
2017-11-27 14:17:22 -0500

    Resolve Conflicts
    Resolved conficts

--- README.md
+++ README.md
@@ -19,27 +19,34 @@ The UCD Informatica Plugin is an automation plugin that connects to specific rep
     Create Folder
     Create Static Deployment Group
     Deploy Deployment Group
+    Folder Migration
     Import Objects
     Roll Back Deployment Group
     Run PMREP Command
     Validate Deployment Group
-	Folder Migration
+    Validate Folder Migration
 
 
 ### Compatibility
-	The IBM UrbanCode Deploy automation plug-in works with Informatica Version 9.6 and later.
-	This plug-in requires version 6.1.1 or later of IBM UrbanCode Deploy.
+
+    The IBM UrbanCode Deploy automation plug-in works with Informatica Version 9.6 and later.
+    This plug-in requires version 6.1.1 or later of IBM UrbanCode Deploy.
 
 ### Installation
-	The packaged zip is located in the dist folder. No special steps are required for installation.
-	See Installing plug-ins in UrbanCode Deploy. Download this zip file if you wish to skip the
-	manual build step. Otherwise, download the entire Information-UCD and
-	run the "ant" command in the top level folder. This should compile the code and create
-	a new distributable zip within the dist folder. Use this command if you wish to make
-	your own changes to the plugin.
+
+    The packaged zip is located in the dist folder. No special steps are required for installation.
+    See Installing plug-ins in UrbanCode Deploy. Download this zip file if you wish to skip the
+    manual build step. Otherwise, download the entire Information-UCD and
+    run the "ant" command in the top level folder. This should compile the code and create
+    a new distributable zip within the dist folder. Use this command if you wish to make
+    your own changes to the plugin.
+
 ### History
 
-    Version 27 - Added Folder Migration Step
+    Version 27
+        Added Folder Migration and Validate Folder Migration Steps
+    Version 26
+        Issue #28 - Property "Security Domain" missing on several steps.
     Version 25
         Issue #26 - Security Domain property added to Apply Label step.
     Version 24
@@ -87,17 +94,17 @@ The UCD Informatica Plugin is an automation plugin that connects to specific rep
         - On failure, the step will now terminate correctly. The stacktrace will be printed for debugging purposes.
     Version 12
         Create Folder step now correctly uses the -shared folder argument.
-	Version 11
-		The following feature is included in version 11 of the Informatica plug-in:
-		Implemented the PMREP command "Create Folder".
-	Version 10
-		The following features are included in first update to Informatica plug-in:
-		Added "Destination Informatica Folder(s)" property to the Deploy Deployment Group step.
-		Renamed "Informatica Folder(s)" to "Source Informatica Folders(s)" for clarity.
-		Updated "Source Informatica Folder(s)" description to reflect these changes.
-	Version 9
-		The following features are included in the initial release of the plug-in:
-		Create static and dynamic deployment groups
-		Deploy, validate, and roll back deployment groups
-		Apply labels to objects in a target server
-		Import objects from XML files with necessary connect and exit commands and control files
+    Version 11
+        The following feature is included in version 11 of the Informatica plug-in:
+        Implemented the PMREP command "Create Folder".
+    Version 10
+        The following features are included in first update to Informatica plug-in:
+        Added "Destination Informatica Folder(s)" property to the Deploy Deployment Group step.
+        Renamed "Informatica Folder(s)" to "Source Informatica Folders(s)" for clarity.
+        Updated "Source Informatica Folder(s)" description to reflect these changes.
+    Version 9
+        The following features are included in the initial release of the plug-in:
+        Create static and dynamic deployment groups
+        Deploy, validate, and roll back deployment groups
+        Apply labels to objects in a target server
+        Import objects from XML files with necessary connect and exit commands and control files
--- src/main/scripts/validate_folder_migration.groovy
+++ src/main/scripts/validate_folder_migration.groovy
@@ -1,5 +1,5 @@
 /**
- * © Copyright IBM Corporation 2015, 2017.
+ * (c) Copyright IBM Corporation 2017.
  * This is licensed under the following license.
  * The Eclipse Public 1.0 License (http://www.eclipse.org/legal/epl-v10.html)
  * U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
@@ -18,11 +18,11 @@ final def stepProps = new Properties();
 final def inputPropsFile = new File(args[0]);
 final def inputPropsStream = null;
 try {
-	inputPropsStream = new FileInputStream(inputPropsFile);
-	stepProps.load(inputPropsStream);
+    inputPropsStream = new FileInputStream(inputPropsFile);
+    stepProps.load(inputPropsStream);
 }
 catch (IOException e) {
-	throw new RuntimeException(e);
+    throw new RuntimeException(e);
 }
 
 
@@ -51,212 +51,202 @@ def process
 
 
 // script for Get the list of all workflows
-	def script = new File(inputFile)
-		script << "connect -r $tarrepo -n $tarusername -x $tarpassword "
+def script = new File(inputFile)
+script << "connect -r $tarrepo -n $tarusername -x $tarpassword "
 
-	if (tarsecurityDomain){
-		script << "-s $tarsecurityDomain "
-	}
-	if (tardomain) {
-		script << "-d $tardomain $LS"
-	
-	} else {
-		script << "-h $tarhost -o $tarport $LS"
-	}
+if (tarsecurityDomain){
+    script << "-s $tarsecurityDomain "
+}
+if (tardomain) {
+    script << "-d $tardomain $LS"
+} else {
+    script << "-h $tarhost -o $tarport $LS"
+}
 
-	for (int i = 0; i < tarfolder.size(); i++) {
-		script << "listobjects -o workflow -f \"${tarfolder[i]}\" $LS"
+for (int i = 0; i < tarfolder.size(); i++) {
+    script << "listobjects -o workflow -f \"${tarfolder[i]}\" $LS"
+}
 
-	}
+println('list of object script content:')
+script.eachLine { line -> println(line) }
+println('')
 
-		println('list of object script content:')
-		script.eachLine { line -> println(line) }
-		println('')
+def exitCode = 0
+def command = []
+if (infaHome != null && infaHome != "") {
+    command.add(infaHome + File.separator + "server" + File.separator + "bin" + File.separator + "pmrep");
+} else {
+  	command.add('pmrep')
+}
 
-	def exitCode = 0
-	def command = []
-	if (infaHome != null && infaHome != "") {
-		command.add(infaHome + File.separator + "server" + File.separator + "bin" + File.separator + "pmrep");
-	} else {
-	  	command.add('pmrep')
-	}
+command.add('run')
+command.add('-o')
+command.add(outputFile)
+command.add('-f')
+command.add(inputFile)
+command.add('-e')
+command.add('-s')
 
-	command.add('run')
-	command.add('-o')
-	command.add(outputFile)
-	command.add('-f')
-	command.add(inputFile)
-	command.add('-e')
-	command.add('-s')
-
-	println('command:' + command )
-	println(command.join(' '))
-	println('')
+println('command:' + command )
+println(command.join(' '))
+println('')
 
 def procBuilder = new ProcessBuilder(command as String[]);
-  if (infaHome != null && infaHome != "") {
-	  def env = procBuilder.environment();
-		  env.put("INFA_HOME", infaHome);
+    if (infaHome != null && infaHome != "") {
+        def env = procBuilder.environment();
+        env.put("INFA_HOME", infaHome);
 
-  if (env.get("LD_LIBRARY_PATH") != null && env.get("LD_LIBRARY_PATH") != "") {
-		  env.put("LD_LIBRARY_PATH", env.get("LD_LIBRARY_PATH") + File.pathSeparator + infaHome + File.separator + "server" + File.separator + "bin");
-  	} else {
-		  env.put("LD_LIBRARY_PATH", infaHome + File.separator + "server" + File.separator + "bin");
+    if (env.get("LD_LIBRARY_PATH") != null && env.get("LD_LIBRARY_PATH") != "") {
+        env.put("LD_LIBRARY_PATH", env.get("LD_LIBRARY_PATH") + File.pathSeparator + infaHome + File.separator + "server" + File.separator + "bin");
+    } else {
+        env.put("LD_LIBRARY_PATH", infaHome + File.separator + "server" + File.separator + "bin");
+    }
+
+    if (env.get("LIBPATH") != null && env.get("LIBPATH") != "") {
+        env.put("LIBPATH", env.get("LIBPATH") + File.pathSeparator + infaHome + File.separator + "server" + File.separator + "bin");
+    } else {
+        env.put("LIBPATH", infaHome + File.separator + "server" + File.separator + "bin");
+    }
+
+    println("With extra  Environment : ");
+    println("INFA_HOME : " + env.get("INFA_HOME"));
+    println("LD_LIBRARY_PATH : " + env.get("LD_LIBRARY_PATH"));
+    println("LIBPATH : " + env.get("LIBPATH"));
   }
-  
-  if (env.get("LIBPATH") != null && env.get("LIBPATH") != "") {
-		  env.put("LIBPATH", env.get("LIBPATH") + File.pathSeparator + infaHome + File.separator + "server" + File.separator + "bin");
-  	}else {
-		  env.put("LIBPATH", infaHome + File.separator + "server" + File.separator + "bin");
-  }
 
-	  println("With extra  Environment : ");
-	  println("INFA_HOME : " + env.get("INFA_HOME"));
-	  println("LD_LIBRARY_PATH : " + env.get("LD_LIBRARY_PATH"));
-	  println("LIBPATH : " + env.get("LIBPATH"));
-  }
+process = procBuilder.start(); command.execute()
+process.consumeProcessOutput(out, out)
+process.getOutputStream().close() // close stdin
+process.waitFor()
 
-  		process = procBuilder.start(); command.execute()
-		process.consumeProcessOutput(out, out)
-		process.getOutputStream().close() // close stdin
-		process.waitFor()
+def output = new File(outputFile)
+Scanner sc = new Scanner(output)
+println('..........pmrep list of object output result.......:')
+lastLine = ""
+while (sc.hasNextLine()) {
+    lastLine = sc.nextLine()
+    println(lastLine)
+}
+println('')
 
-	def output = new File(outputFile)
-		Scanner sc = new Scanner(output)
-		println('..........pmrep list of object output result.......:')
-	lastLine = ""
-	while (sc.hasNextLine()) {
-		lastLine = sc.nextLine()
-		println(lastLine)
-	}
-		println('')
+Scanner sa = new Scanner(output)
 
-	Scanner sa = new Scanner(output)
+def list = []
 
-	def list = []
+while (sa.hasNextLine()){
+    list.add(sa.nextLine());
+}
+sa.close();
 
-	while (sa.hasNextLine()){
-		list.add(sa.nextLine());
-	}
-	sa.close();
-	
 // mapping the each folder as key and each workflow as values.
-	
-	def mapWorkflow = [:]
 
-	def key = "", workflowitems = []
+def mapWorkflow = [:]
 
-	for (int i=0; i<list.size(); i++){
-		if(list[i].contains('workflow -f')){
-			key = list[i].substring(list[i].indexOf('workflow -f')+11,list[i].length() )
-			i++
-		}
-		while(i<list.size() && list[i].startsWith('workflow') && key.size()>0) {
-			workflowitems += list[i].minus('workflow').trim()
-			i++
-		}
-	if(key.size()>0 && workflowitems.size()>0){
-			mapWorkflow[key.trim()]=workflowitems
-			workflowitems =[]
-			key=""
-		}
+def key = "", workflowitems = []
 
-	}
+for (int i=0; i<list.size(); i++){
+    if(list[i].contains('workflow -f')){
+        key = list[i].substring(list[i].indexOf('workflow -f')+11,list[i].length() )
+        i++
+    }
+    while(i<list.size() && list[i].startsWith('workflow') && key.size()>0) {
+        workflowitems += list[i].minus('workflow').trim()
+        i++
+    }
+    if(key.size()>0 && workflowitems.size()>0){
+        mapWorkflow[key.trim()]=workflowitems
+        workflowitems =[]
+        key=""
+    }
+
+}
 
 
 //validate each workflow with in the  each folder
 
-	if (mapWorkflow != null && mapWorkflow.size()>0){   
-	 for (int i = 0; i < tarfolder.size(); i++) {
-			 def workflowItemList = mapWorkflow.get("\""+tarfolder[i]+"\"")
-			 workflowItemList.each{
-				 script <<  "validate -n ${it} -o workflow -f \"${tarfolder[i]}\" -p valid,invalid_after -u $workflowOutputFile -a  $LS"
-	
-			 }
+if (mapWorkflow != null && mapWorkflow.size()>0){
+    for (int i = 0; i < tarfolder.size(); i++) {
+        def workflowItemList = mapWorkflow.get("\""+tarfolder[i]+"\"")
+        workflowItemList.each{
+            script <<  "validate -n ${it} -o workflow -f \"${tarfolder[i]}\" -p valid,invalid_after -u $workflowOutputFile -a  $LS"
+        }
+    }
 
-	 }
-	 if(workflowOutputFile){
-		script << "listobjectdependencies -i $workflowOutputFile -s -p both -u $workflowDependenciesFile -a  $LS"
-		
-	 }
-	 	
-	if (workflowDependenciesFile){
-		script << "validate -i $workflowDependenciesFile -s -k -m validated -p invalid_after  -u $invalidObjectsFile -a $LS"
-	
-	}
-		script << "exit"
+    if(workflowOutputFile){
+        script << "listobjectdependencies -i $workflowOutputFile -s -p both -u $workflowDependenciesFile -a  $LS"
+    }
 
-			println('script content-----------:')
-			script.eachLine { line -> println(line) }
-		    println('')
-	
-	
-	try{
+    if (workflowDependenciesFile){
+    script << "validate -i $workflowDependenciesFile -s -k -m validated -p invalid_after  -u $invalidObjectsFile -a $LS"
+    }
+    script << "exit"
 
-		process = procBuilder.start(); command.execute()
-		process.consumeProcessOutput(out, out)
-		process.getOutputStream().close()
-		process.waitFor()
+    println('script content-----------:')
+    script.eachLine { line -> println(line) }
+    println('')
 
 
-		sc = new Scanner(output)
-			println('-------pmrep validationoutput result----------------:')
-		lastLine = ""
-		while (sc.hasNextLine()) {
-			lastLine = sc.nextLine()
-			println(lastLine)
-		}
-			println('')
+    try{
 
-			
-	if (invalidObjectsFile.exists() && invalidObjectsFile.length() > 0){
-		println "---------------------------------------------------------------------------------------------------------------"
-		println "---------------- Invalid objects found in the repository. Running validate on the objects.---------------------"
-	
-		def invalidlist = []
-			invalidObjectsFile.eachLine{line ->
-				invalidlist.add(line)
-				
-			}
-			
-		for(line in invalidlist){
-			def splitArry = line.split(",")
-			def folderNameList = []
-			for(int i=2; i<splitArry.size()-2; i++){
-				folderNameList.add(splitArry[i])
-					
-			}
-		
-				println("Folder:----" + splitArry[1])
-				println("InvalidObjects Found in:------" + folderNameList.join(","))
-				println("");
-				exitCode = 1
-				
-				}
-			
-			} else {
-			
-				println('')
-				println "----------------No objects found in the repository.---------------------"
+        process = procBuilder.start(); command.execute()
+        process.consumeProcessOutput(out, out)
+        process.getOutputStream().close()
+        process.waitFor()
 
-			}
-		
-		output.delete()
-		script.delete()
-		
-		} catch (Exception ex) {
-			println "--------[Error] Please review the output log and stack trace for information on the error.---------"
-			println ex.printStackTrace()
-			exitCode = 1
-		
-			} finally {
-	
-			System.exit(exitCode)
-		}
 
-	} else {
-		println('')
-		println("-------------No invalid objects found in the repository.---------------")
-		println('')
-		System.exit(exitCode)
+        sc = new Scanner(output)
+        println('-------pmrep validationoutput result----------------:')
+        lastLine = ""
+        while (sc.hasNextLine()) {
+            lastLine = sc.nextLine()
+            println(lastLine)
+        }
+        println('')
+
+
+        if (invalidObjectsFile.exists() && invalidObjectsFile.length() > 0){
+            println "---------------------------------------------------------------------------------------------------------------"
+            println "---------------- Invalid objects found in the repository. Running validate on the objects.---------------------"
+
+            def invalidlist = []
+            invalidObjectsFile.eachLine{line ->
+            invalidlist.add(line)
+            }
+
+            for(line in invalidlist){
+                def splitArry = line.split(",")
+                def folderNameList = []
+                for(int i=2; i<splitArry.size()-2; i++){
+                    folderNameList.add(splitArry[i])
+                }
+
+                println("Folder:----" + splitArry[1])
+                println("InvalidObjects Found in:------" + folderNameList.join(","))
+                println("");
+                exitCode = 1
+            }
+
+        } else {
+
+        println('')
+        println "----------------No objects found in the repository.---------------------"
+
+        }
+
+        output.delete()
+        script.delete()
+
+    } catch (Exception ex) {
+        println "--------[Error] Please review the output log and stack trace for information on the error.---------"
+        println ex.printStackTrace()
+        exitCode = 1
+    } finally {
+        System.exit(exitCode)
+    }
+
+} else {
+    println('')
+    println("-------------No invalid objects found in the repository.---------------")
+    println('')
+    System.exit(exitCode)
 }
